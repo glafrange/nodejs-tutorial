3b1c57b20bf0a7c40372bf5439f9a69a550b8469
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-15 13:01:25 -0400

    lesson 12

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var fs = require('fs');
+var http = require('http');
 
-fs.unlink('./stuff/write-me.txt', () => fs.rmdir('stuff', (err) => console.log(err)) )
+var server = http.createServer(function(req,res){
+  console.log("request was made " + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hey ninjas');
+});
+
+server.listen(1337, '127.0.0.1');
+console.log('listening to port 1337');
