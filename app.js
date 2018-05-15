d21cb35893b0a2a91f5cc0b2c8a52f7baa811228
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-15 12:30:00 -0400

    lesson 9

--- app.js
+++ app.js
@@ -1,22 +1,5 @@
-var events = require('events');
-var util = require('util');
+var fs = require('fs');
 
-var Person = function(name){
-  this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person('James');
-var mary = new Person('Mary');
-var ryu = new Person('Ryu');
-var people = [james, mary, ryu];
-
-people.forEach(function(person){
-  person.on('speak', function(msg){
-    console.log(person.name + ' said: ' + msg);
-  });
+fs.readFile('read-me.txt', 'utf8', function(err, data){
+  fs.writeFile('write-me.txt', data, () => null);
 });
-
-james.emit('speak', 'hey dudes');
-ryu.emit('speak', 'I want a curry');
