4994c514b4a3b065db1f95c2e18171b4c1f5258d
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-15 11:42:00 -0400

    lesson 8

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
-console.log(stuff.adder(3,2));
-console.log(stuff.adder(stuff.pi,5));
+var Person = function(name){
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('James');
+var mary = new Person('Mary');
+var ryu = new Person('Ryu');
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+  person.on('speak', function(msg){
+    console.log(person.name + ' said: ' + msg);
+  });
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry');
