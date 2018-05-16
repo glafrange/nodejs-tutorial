91d5d930a461f086bc3798b98c3f814147c850e7
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-16 14:22:07 -0400

    lesson 18 - Serving JSON data

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req,res){
   console.log("request was made " + req.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
-  myReadStream.pipe(res);
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var myObj = {
+    name: 'Ryu',
+    job: 'Ninja',
+    age: 29
+  };
+  res.end(JSON.stringify(myObj));
 });
 
 server.listen(1337, '127.0.0.1');
