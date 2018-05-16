b9a498c373a9ea0a5b291f68092dbb9507cbbe6c
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-16 12:48:14 -0400

    lesson 14

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req,res){
-  console.log("request was made " + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hey ninjas');
+var myReadStream = fs.createReadStream(__dirname + '/read-me.txt', 'utf-8');
+
+myReadStream.on('data', function(chunk){
+  console.log('new chunk received');
+  console.log(chunk);
 });
 
-server.listen(1337, '127.0.0.1');
-console.log('listening to port 1337');
+
+
+// var http = require('http');
+
+// var server = http.createServer(function(req,res){
+//   console.log("request was made " + req.url);
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hey ninjas');
+// });
+
+// server.listen(1337, '127.0.0.1');
+// console.log('listening to port 1337');
