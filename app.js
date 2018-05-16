56676e9bab2345e1c5a8cf79728bc4311774ad8d
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-16 19:33:05 -0400

    lesson 25 - template engines

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('this is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-  res.send('this is the contact page');
+  res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', function(req, res){
-  res.send('You requested to see a profile with the name of ' + req.params.name);
+  var data = {
+    age: 29,
+    job: 'ninja'
+  };
+  res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(1337);
